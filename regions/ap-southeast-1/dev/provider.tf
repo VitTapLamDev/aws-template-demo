ad18338928bea0e3525c748f5a961f32e723da7a
VitTapLamDev <vietlcvn123@gmail.com>
2025-03-12 21:15:17 +0700

    Fix aws provider

--- regions/ap-southeast-1/dev/provider.tf
+++ regions/ap-southeast-1/dev/provider.tf
@@ -1,10 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.0"
-        access_key = var.access_key
-        secret_key = var.secret_key
-    }
-  }
+provider "aws" {
+  region = "ap-southeast-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  
 }
